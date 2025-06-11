3b840a080059c5bcca7e8d2e8f5c4e97bbcd5427
ElWillys <55439170+ElWillys@users.noreply.github.com>
2025-06-11 02:22:35 +0200

    Añadidos metodos findByStatusAndUserId, findByInitialDateBetween

--- src/main/java/org/ediae/tfm/crmapi/repository/TaskRepository.java
+++ src/main/java/org/ediae/tfm/crmapi/repository/TaskRepository.java
@@ -6,6 +6,7 @@ import org.ediae.tfm.crmapi.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -13,4 +14,7 @@ public interface TaskRepository extends JpaRepository <Task, Long> {
     List<Task> findByStatus(Task.Status status);
     List<Task> findByUser(AppUser user);
     List<Task> findByCustomer(Customer customer);
+
+    List<Task> findByStatusAndUserId(Task.Status status, Long userId);
+    List<Task> findByInitialDateBetween(LocalDate start, LocalDate end);
 }
