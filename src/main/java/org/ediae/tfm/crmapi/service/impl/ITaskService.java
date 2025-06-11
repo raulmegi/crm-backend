8a35cd3f2d621b84a6d1095a718b6b974425c745
ElWillys <55439170+ElWillys@users.noreply.github.com>
2025-06-11 02:36:55 +0200

    Añadidos metodos en ITaskService

---
+++ src/main/java/org/ediae/tfm/crmapi/service/impl/ITaskService.java
@@ -0,0 +1,15 @@
+package org.ediae.tfm.crmapi.service.impl;
+
+import org.ediae.tfm.crmapi.entity.Task;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ITaskService {
+    Task create(Task task);
+    List<Task> findAll();
+    Optional<Task> findById(Long id);
+    Task update(Task task);
+    void delete(Long id);
+    List<Task> findByStatus(Task.Status status);
+}
