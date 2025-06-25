bba3a393662f6bbdf195db840e8b2f2aa06449f2
raulmegi <mat.raulmegias@gmail.com>
2025-06-25 19:16:42 +0200

    ModelMap y control de errores implementados en customer.

--- src/main/java/org/ediae/tfm/crmapi/constant/GeneralConstants.java
+++ src/main/java/org/ediae/tfm/crmapi/constant/GeneralConstants.java
@@ -47,10 +47,20 @@ public class GeneralConstants {
     public static final String APPUSER_NAME_SEARCH_ERROR_MESSAGE = "Error al buscar usuario por nombre";
     public static final String APPUSER_EMAIL_IN_USE_ERROR_MESSAGE = "Este correo electrónico ya existe";
 
+    // Códigos y mensajes específicos para clientes
+    public static final int CUSTOMER_NOT_FOUND_CODE = 300;
+    public static final int CUSTOMER_CREATION_ERROR_CODE = 301;
+    public static final int CUSTOMER_UPDATE_ERROR_CODE = 302;
+    public static final int CUSTOMER_DELETE_ERROR_CODE = 303;
+
+
+    public static final String CUSTOMER_CREATION_ERROR_MESSAGE = "No se pudo crear el cliente";
+    public static final String CUSTOMER_UPDATE_ERROR_MESSAGE = "No se pudo actualizar el cliente";
+    public static final String CUSTOMER_DELETE_ERROR_MESSAGE = "No se pudo eliminar el cliente";
+    public static final String CUSTOMER_NOT_FOUND_MESSAGE = "El cliente no ha sido encontrado";
+
 
     // Otros posibles
-    public static final int CUSTOMER_NOT_FOUND_CODE = 300;
-    public static final String CUSTOMER_NOT_FOUND_MESSAGE = "El cliente no ha sido encontrado";
 
     public static final String GENERAL_ERROR_MESSAGE = "Error inesperado en el servidor";
     public static final String BD_ERROR_MESSAGE = "Error de conexión a base de datos";
--- src/main/java/org/ediae/tfm/crmapi/repository/CustomerRepository.java
+++ src/main/java/org/ediae/tfm/crmapi/repository/CustomerRepository.java
@@ -3,7 +3,7 @@ package org.ediae.tfm.crmapi.repository;
 import org.ediae.tfm.crmapi.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository <Customer, Integer> {
+public interface CustomerRepository extends JpaRepository <Customer, Long> {
 
 
 }
