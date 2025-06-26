7b39ac7c5d10459e882efce4239d0f0f939d5c7f
AlejandroTorresRuiz28 <alextorruiz2812@gmail.com>
2025-06-26 22:51:25 +0200

    creado Zonas

---
+++ src/main/java/org/ediae/tfm/crmapi/controller/ZoneController.java
@@ -0,0 +1,36 @@
+package org.ediae.tfm.crmapi.controller;
+
+
+import org.ediae.tfm.crmapi.service.IZoneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/zone")
+public class ZoneController {
+
+    @Autowired
+    private IZoneService zoneService;
+
+    @GetMapping("/listarZonas")
+    public ModelMap findAllBrands() {
+        try {
+            return GeneralUtilsController.crearRespuestaModelMapOk(zoneService.findAllZones());
+        } catch (Exception ex) {
+            return GeneralUtilsController.crearRespuestaModelMapError(ex);
+        }
+    }
+
+    @GetMapping("/encontrarZona/{id}")
+    public ModelMap getZoneById(@PathVariable Long id) {
+        try {
+            return GeneralUtilsController.crearRespuestaModelMapOk(zoneService.findZoneById(id));
+        }catch (Exception ex) {
+            return GeneralUtilsController.crearRespuestaModelMapError(ex);
+        }
+    }
+}
