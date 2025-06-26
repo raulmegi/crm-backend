ec2a85f8bf0d06c94c5935129e27b068a9829692
AlejandroTorresRuiz28 <alextorruiz2812@gmail.com>
2025-06-27 01:12:32 +0200

    añadido CrossOrigin a mis Controller

--- src/main/java/org/ediae/tfm/crmapi/controller/BrandController.java
+++ src/main/java/org/ediae/tfm/crmapi/controller/BrandController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/brand")
 public class BrandController {
 
--- src/main/java/org/ediae/tfm/crmapi/controller/ZoneController.java
+++ src/main/java/org/ediae/tfm/crmapi/controller/ZoneController.java
@@ -4,12 +4,10 @@ package org.ediae.tfm.crmapi.controller;
 import org.ediae.tfm.crmapi.service.IZoneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/zone")
 public class ZoneController {
 
