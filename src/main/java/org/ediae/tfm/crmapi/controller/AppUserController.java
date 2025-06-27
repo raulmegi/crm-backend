2a0309930d42e50bec425c04eec2587530a44bfe
ElWillys <guillermorejonroldan@gmail.com>
2025-06-27 20:16:02 +0200

    añadido find by user

--- src/main/java/org/ediae/tfm/crmapi/controller/AppUserController.java
+++ src/main/java/org/ediae/tfm/crmapi/controller/AppUserController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/appUser")
-@CrossOrigin(origins = "http://localhost:4200")
 public class AppUserController {
 
     @Autowired
