93b439c7bcc99b325623deb7b6bd83902f8e6461
Narrmora <29nroman11@gmail.com>
2025-07-06 22:13:59 +0200

    final security updates

--- src/main/java/org/ediae/tfm/crmapi/config/SecurityConfig.java
+++ src/main/java/org/ediae/tfm/crmapi/config/SecurityConfig.java
@@ -11,8 +11,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.apache.commons.lang3.BooleanUtils.and;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
