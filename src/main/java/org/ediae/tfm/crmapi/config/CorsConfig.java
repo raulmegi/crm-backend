b5731145525c7b66f27fe2790087230f09c95004
raulmegi <mat.raulmegias@gmail.com>
2025-07-16 10:18:31 +0200

    Update CorsConfig.java

--- src/main/java/org/ediae/tfm/crmapi/config/CorsConfig.java
+++ src/main/java/org/ediae/tfm/crmapi/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("*")
                         .allowedHeaders("*")
                         .allowCredentials(true)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
